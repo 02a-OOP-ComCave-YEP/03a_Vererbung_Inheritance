30690b3308b7eb25ba3766bc6e4b98ba8ce0a197
PiSera1 <kevinwellens24@gmail.com>
2025-01-21 12:12:18 +0100

    1. / Cat() / Tomcat()

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,15 +5,13 @@ public class Cat {
     private String firstName;
     private String furColor;
     private int age;
-    private boolean isFemale;
 
     private int counter;
 
-    public Cat(String firstName, String furColor, int age, boolean isFemale) {
+    public Cat(String firstName, String furColor, int age) {
         this.firstName = firstName;
         this.furColor = furColor;
         this.age = age;
-        this.isFemale = isFemale;
     }
 
     /** Getter */
@@ -29,14 +27,9 @@ public class Cat {
     }
 
     public String getAge() {
-        if (isFemale) {
            return checkCompliance();
-        } else {
-            return Integer.toString(age); 
-        }
     }
 
-    
     private String checkCompliance(){
 
        counter++;
@@ -49,14 +42,10 @@ public class Cat {
             case 3:
                 return "Talk to the hand!"; // 3rd time 
             default:
-                return "1#!?&&%"; //4th ++
+                return "fuck off"; //4th ++
         }
 
     }
-
-
-
-  
 
 
 }
