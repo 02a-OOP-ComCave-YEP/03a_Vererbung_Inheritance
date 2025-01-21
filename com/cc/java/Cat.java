70222ae4c1180216e6c457d61175ee54dacf34d8
PiSera1 <kevinwellens24@gmail.com>
2025-01-21 15:27:51 +0100

    3. / extends / Cat / Tomcat

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,29 +1,12 @@
 package com.cc.java;
 
-public class Cat {
-
-    private String firstName;
-    private String furColor;
-    private int age;
+public class Cat extends SuperCat {
 
     private int counter;
 
     public Cat(String firstName, String furColor, int age) {
-        this.firstName = firstName;
-        this.furColor = furColor;
-        this.age = age;
-    }
-
-    /** Getter */
-    public String getStringAttributes(String op) {
-      switch (op) {
-        case "#name":
-            return firstName;
-        case "#color":
-            return furColor;
-        default:
-            return "ERROR";
-      }
+        super(firstName, furColor, age);
+        
     }
 
     public String getAge() {
@@ -42,7 +25,7 @@ public class Cat {
             case 3:
                 return "Talk to the hand!"; // 3rd time 
             default:
-                return "Fuck off"; //4th ++
+                return "Fuck off!"; //4th ++
         }
 
     }
